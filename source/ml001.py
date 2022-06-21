b433c5afa7f184fe44d62d9af1de657efbcb4841
baeseongho <bae9808@gmail.com>
2022-06-21 11:59:13 +0900

    20220621 Second Commit...

--- source/ml001.py
+++ source/ml001.py
@@ -1,40 +1,25 @@
-# Sucess
+import os
+import  glob
 import pandas as pd
-import os 
-m_store = pd.read_csv('m_store.csv')
-len(m_store)
-m_store.head()
+import numpy as np
 
-m_area = pd.read_csv('m_area.csv')
-m_area
+# 작업 폴더 지정 ..
+# 대리점 및 지역 코드 데이터 프레임 생성 ...
+path = '/Users/baeseongho/PycharmProjects/pythonML/'
+m_store = pd.read_csv(path+'m_store.csv')
+m_area = pd.read_csv(path+'m_area.csv')
 
-tbl_order_4 = pd.read_csv('tbl_order_202104.csv')
-tbl_order_4
-
-tbl_order_5 = pd.read_csv('tbl_order_202105.csv')
-tbl_order_5
-
-tbl_order_6 = pd.read_csv('tbl_order_202106.csv')
-tbl_order_6
-
-order_all = pd.concat([tbl_order_4,tbl_order_5,tbl_order_6],ignore_index=True)
-order_all
-
-current_dir = os.getcwd()
-os.listdir(current_dir)
-
-tbl_order_file = os.path.join(current_dir,'tbl_order_*.csv')
-tbl_order_file
-
-import glob
+# 작업 폴더에 tbl_order_로 시작사는 csv 매출 데이터 파일 목록을 가지는 변수 생성 ..
+os.listdir(path)
+tbl_order_file = os.path.join(path,'tbl_order_*.csv')
 tbl_order_files = glob.glob(tbl_order_file)
-tbl_order_files
 
+# 폴더에 매출 데이터 일괄로 order_all 로 생성 ..
 order_all = pd.DataFrame()
 for file in tbl_order_files :
     order_data = pd.read_csv(file)
-    print(f'{file} : {len(order_data)}')
-    order_all = pd.concat([order_all, order_data],ignore_index=True)
+    print((f'{file} : {len(order_data)}'))
+    order_all = pd.concat([order_all,order_data],ignore_index=True)
 
-order_all
 order_all.isnull().sum()
+order_all.describe()
