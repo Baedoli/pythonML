1eb0563955283c7d96616efca2af601a9284780c
baeseongho <bae9808@gmail.com>
2022-06-21 21:27:51 +0900

    20220621 Last Commit...

--- source/ml002.py
+++ source/ml002.py
@@ -7,4 +7,3 @@ order_data = pd.read_csv(path+'/output_data/order_data.csv')
 order_data.head()
 order_data = order_data.loc[(order_data['status']==1)|(order_data['status']==2)]
 order_data.head()
-order_data.columns
