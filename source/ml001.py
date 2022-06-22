627234c5751ecc8a8a0f52dfe53ea21ce3289d4f
baeseongho <bae9808@gmail.com>
2022-06-22 15:57:40 +0900

    20220622 First Commit...

--- source/ml001.py
+++ source/ml001.py
@@ -1,7 +1,6 @@
 import os
-import  glob
+import glob
 import pandas as pd
-import numpy as np
 
 # 작업 폴더 지정 ..
 # 대리점 및 지역 코드 데이터 프레임 생성 ...
@@ -23,3 +22,30 @@ for file in tbl_order_files :
 
 order_all.isnull().sum()
 order_all.describe()
+len(order_all)
+
+order_all['total_amount'].describe()
+
+# 불필요한 마스터 데이터 제거 999 ( 유지보수 지점용 데이터 )
+order_data = order_all.loc[order_all['store_id']!=999]
+
+# 마스터 데이터 결함 ..
+order_data = pd.merge(order_data,m_store,on='store_id',how='left')
+order_data = pd.merge(order_data,m_area,on='area_cd',how='left')
+
+# 수령방법 이름 설정 ..
+order_data.loc[order_data['takeout_flag']==0,'takeout_name'] = 'delivery'
+order_data.loc[order_data['takeout_flag']==1,'takeout_name'] = 'takeout'
+
+# 주문상태 이름 설정 ..
+order_data.loc[order_data['status']==0,'status_name'] = '주문 접수'
+order_data.loc[order_data['status']==1,'status_name'] = '지불 완료'
+order_data.loc[order_data['status']==2,'status_name'] = '배달 완료'
+order_data.loc[order_data['status']==9,'status_name'] = '주문 취소'
+
+#  파일 저장  ...
+output_dir = os.path.join(path,'output_data')
+os.makedirs(output_dir,exist_ok=True)
+output_file = os.path.join(output_dir,'order_data.csv')
+order_data.to_csv(output_file,index=False)
+
--- source/ml002.py
+++ source/ml002.py
@@ -1,9 +1,93 @@
 
 import  pandas as pd
 import os
+import warnings
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-path = os.getcwd()
+
+plt.rc('font',family='AppleGothic')
+plt.rc('axes',unicode_minus=False)
+
+warnings.filterwarnings('ignore')
+# path = os.getcwd()
+path =  '/Users/baeseongho/PycharmProjects/pythonML'
 order_data = pd.read_csv(path+'/output_data/order_data.csv')
-order_data.head()
 order_data = order_data.loc[(order_data['status']==1)|(order_data['status']==2)]
 order_data.head()
+order_data.columns
+len(order_data)
+
+analyze_data = order_data[['store_id', 'customer_id', 'coupon_cd', 'order_accept_date', 'delivered_date', 'total_amount',
+                           'store_name', 'wide_area', 'narrow_area', 'takeout_name', 'status_name']]
+
+analyze_data.shape
+analyze_data.head()
+
+analyze_data.describe()
+analyze_data.dtypes
+
+analyze_data[['store_id','coupon_cd']] = analyze_data[['store_id','coupon_cd']].astype(str)
+analyze_data.dtypes
+
+# 주문일자 타입을 문자에서 날짜 형으로 바꾸고 .. 월별로 자동 변환한 칼럼을 하나 추가 함 ..
+analyze_data['order_accept_date'] = pd.to_datetime(analyze_data['order_accept_date'])
+analyze_data['order_accept_month'] = analyze_data['order_accept_date'].dt.strftime('%Y%m')
+analyze_data[['order_accept_date','order_accept_month']].head()
+
+analyze_data['delivered_date'] = pd.to_datetime(analyze_data['delivered_date'])
+analyze_data['delivered_month'] = analyze_data['delivered_date'].dt.strftime('%Y%m')
+analyze_data[['delivered_date','delivered_month']].head()
+
+month_data = analyze_data.groupby('order_accept_month')
+month_data.head()
+month_data.describe()
+month_data.sum()
+
+plt.hist(analyze_data['total_amount'],bins=24)
+plt.show()
+
+pre_data = pd.pivot_table(analyze_data,index='order_accept_month',columns='narrow_area',values='total_amount',aggfunc='mean')
+pre_data
+
+plt.plot(list(pre_data.index),pre_data['서울'],label='서울')
+plt.plot(list(pre_data.index),pre_data['부산'],label='부산')
+plt.plot(list(pre_data.index),pre_data['대전'],label='대전')
+plt.plot(list(pre_data.index),pre_data['광주'],label='광주')
+plt.plot(list(pre_data.index),pre_data['세종'],label='세종')
+plt.show()
+
+store_clustring = analyze_data.groupby('store_id').agg(['size','mean','median','max','min'])['total_amount']
+store_clustring.reset_index(inplace=True,drop=True)
+len(store_clustring)
+store_clustring.head()
+hexbin = sns.jointplot(x='mean',y='size',data=store_clustring,kind='hex')
+plt.show()
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+store_clustring_sc = sc.fit_transform(store_clustring)
+
+kmeans = KMeans(n_clusters=4, random_state=0)
+clusters = kmeans.fit(store_clustring_sc)
+store_clustring['cluster'] = clusters.labels_
+print(store_clustring['cluster'].unique())
+store_clustring.head()
+
+store_clustring.columns = ['월 건수','월 평균값','월 중앙값','월 최댓값','월 최소값','cluster']
+store_clustring.head()
+store_clustring.groupby('cluster').count()
+store_clustring.groupby('cluster').mean()
+
+from sklearn.manifold import TSNE
+tsne = TSNE(n_components=2,random_state=0)
+x = tsne.fit_transform(store_clustring_sc)
+
+tsne_df = pd.DataFrame(x)
+tsne_df['cluster'] = store_clustring['cluster']
+tsne_df.columns = ['axis_0','axis_1','cluster']
+tsne_df.head()
+
+tsne_graph = sns.scatterplot(x='axis_0',y='axis_1',hue='cluster',data=tsne_df)
+plt.show()
